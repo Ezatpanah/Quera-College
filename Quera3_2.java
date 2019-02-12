ea5cb424c4bc77cc218b315089a6fae938d6a65e
Reyhaneh Ezatpanah <ezatpanah@gmail.com>
2019-02-12 10:00:49 +0330

    Create Quera3_2.java

---
+++ Quera3_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt(), b = scanner.nextInt(),c = scanner.nextInt(), d = scanner.nextInt(),e = scanner.nextInt(), f = scanner.nextInt();
+		
+		int a1=1;
+		int b1=1;
+		int c1=2;
+		int d1=2;
+		int e1=2;
+		int f1=8;
+		
+		a1 = a1-a;
+		b1 = b1-b;
+		c1 = c1-c;
+		d1 = d1-d;
+		e1 = e1-e;
+		f1 = f1-f;
+		
+		System.out.println(a1 + " " + b1 + " " + c1 +" " + d1 + " " + e1 + " " + f1);
+    }
+
+}
